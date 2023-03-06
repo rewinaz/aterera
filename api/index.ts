abfe5ba8b9338da36b0e8cc197ad4ed761235a81
rewina zerou <rewinazerou6@gmail.com>
2023-03-06 12:48:05 +0300

    express app code refactoring

--- api/index.ts
+++ api/index.ts
@@ -1,33 +1,30 @@
 import mongoose from "mongoose";
-import morgan from "morgan";
-import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import morgan from "morgan";
+import server from "./startup/server";
 // Routes
 import authRoute from "./routes/auth";
 import userRoute from "./routes/users";
 import noteRoute from "./routes/notes";
 import trashRoute from "./routes/trash";
 
-const app = express();
 const PORT = 3000 | Number(process.env.PORT);
 
 dotenv.config();
-app.use(
+server.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
   })
 );
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(morgan("tiny"));
+server.use(morgan("tiny"));
 
 // Routes
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/note", noteRoute);
-app.use("/api/v1/trash", trashRoute);
+server.use("/api/v1/auth", authRoute);
+server.use("/api/v1/user", userRoute);
+server.use("/api/v1/note", noteRoute);
+server.use("/api/v1/trash", trashRoute);
 
 mongoose.set("strictQuery", true);
 mongoose
@@ -39,6 +36,6 @@ mongoose
     console.log(err);
   });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
